e6060d4e6be571f4f2d2b6a2c28934a6d488625a
Roope <Roope@DESKTOP-U00SOB5>
2017-01-14 18:45:34 +0200

    Add project files.

---
+++ T1/T1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int luku;
+
+            Console.Write("Anna luku > ");
+
+            luku = int.Parse(Console.ReadLine());
+
+            if (luku == 1 || luku == 2 || luku == 3)
+            {
+                Console.WriteLine("Annoit luvun " + luku);
+            }
+            else
+	{
+                Console.WriteLine("Joku muu luku ");
+            }
+           
+        }
+    }
+}
