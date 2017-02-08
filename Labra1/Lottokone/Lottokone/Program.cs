ea9aec34a1518f45c88be4f8fc5ded066eeecdb7
K8980 <K8980@student.jamk.fi>
2017-02-08 12:23:37 +0200

    lab9 done

---
+++ Labra1/Lottokone/Lottokone/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// liuewr glihearg hlierahgieahr göhea
+namespace Lottokone
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
