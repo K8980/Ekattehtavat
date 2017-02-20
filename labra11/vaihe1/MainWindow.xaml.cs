5856ef86ce02be44d4ad2648644134257d5d0d74
K8980 <K8980@student.jamk.fi>
2017-02-20 10:14:56 +0200

    lab11 done

---
+++ labra11/vaihe1/MainWindow.xaml.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using JAMK.ICT;
+
+
+namespace vaihe1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        // koska useampi metodi = tapahtumankäsittelija tulee käyttämään näitä muttujia niin
+        // määritellään luokan tasolle luokan jäsenmuuttujiksi
+        HockeyLeague liiga;
+        ObservableCollection<HockeyTeam> joukkueet;
+        int counter = 0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            IniMyStuff();
+        }
+
+        private void IniMyStuff()
+        {
+            List<string> muuvit = new List<string>();
+            muuvit.Add("Halloween");
+            muuvit.Add("Jaws");
+            muuvit.Add("Star Wars");
+            muuvit.Add("Pahat pojat");
+            cmbMovies.ItemsSource = muuvit;
+
+            // haetaan smliiga-joukkueet
+            liiga = new HockeyLeague();
+            joukkueet = liiga.GetTeams();
+            cmbTeams.ItemsSource = joukkueet;
+
+
+        }
+
+        private void btnBind_Click(object sender, RoutedEventArgs e)
+        {
+            // määritellään stackpanelin DataContext
+            // Demo1: Datacontekstina on olio
+            //  HockeyTeam tiimi = new HockeyTeam("KeuPa", "Keuruu");
+            //  spRight.DataContext = tiimi;
+
+            //demo2: kytketään olio-kokoelman 1. olioon
+            spRight.DataContext = joukkueet[counter];
+        }
+
+        private void btnBackward_Click(object sender, RoutedEventArgs e)
+        {
+            if (counter > 0)
+            {
+                counter--;
+                btnBind_Click(sender, e);
+            }
+        }
+
+        private void btnForward_Click(object sender, RoutedEventArgs e)
+        {
+            if (counter < joukkueet.Count - 1)
+            {
+                counter++;
+                btnBind_Click(sender, e);
+            }
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (txtJoukkueNimi.Text.Length > 0 && txtJoukkueKaupunki.Text.Length > 0)
+                {
+                    HockeyTeam temp = new HockeyTeam(txtJoukkueNimi.Text, txtJoukkueKaupunki.Text);
+                    liiga.AddTeam(temp);
+                    txtJoukkueNimi.Text = "";
+                    txtJoukkueKaupunki.Text = "";
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message + "\n\n" + ex, "error while adding team");
+            }
+        }
+    }
+}
